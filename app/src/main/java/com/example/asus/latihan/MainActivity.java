7c0ec255d06b40d4c20da7dc558eaf22bfe4ab81
Awijie138 <mutawallysyarawy@gmail.com>
2017-11-26 16:45:42 +0800

    baru

--- app/src/main/java/com/example/asus/latihan/MainActivity.java
+++ app/src/main/java/com/example/asus/latihan/MainActivity.java
@@ -1,31 +1,34 @@
 package com.example.asus.latihan;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     int a=0,b=0;
+    TextView quantityTextViewA,quantityTextViewB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        quantityTextViewA = (TextView) findViewById(
+                R.id.score_A
+        );
+        quantityTextViewB = (TextView) findViewById(
+                R.id.score_B
+        );
     }
 
     public void displayA(int score){
-        TextView quantityTextView = (TextView) findViewById(
-                R.id.score_A
-        );
-        quantityTextView.setText(""+score);
+        quantityTextViewA.setText(""+score);
     }
 
     public void displayB(int score){
-        TextView quantityTextView = (TextView) findViewById(
-                R.id.score_B
-        );
-        quantityTextView.setText(""+score);
+        quantityTextViewB.setText(""+score);
     }
 
     public void reset (View view){
@@ -33,35 +36,59 @@ public class MainActivity extends AppCompatActivity {
         b=0;
         displayB(b);
         displayA(a);
+        Toast(2);
+    }
+
+    public void Toast(int option){
+        switch (option) {
+            case 1:   Toast.makeText(this, "The Score Has Been Change", Toast.LENGTH_SHORT).show();
+            break;
+            case 2:   Toast.makeText(this, "The Score Has Been Reset", Toast.LENGTH_SHORT).show();
+            break;
+        }
     }
 
     public void point2A(View view){
         a+=2;
         displayA(a);
+        Toast(1);
     }
 
     public void point3A(View view){
         a+=3;
         displayA(a);
+        Toast(1);
     }
 
     public void freethrowA(View view){
         a+=1;
         displayA(a);
+        Toast(1);
     }
 
     public void point2B(View view){
         b+=2;
         displayB(b);
+        Toast(1);
     }
 
     public void point3B(View view){
         b+=3;
         displayB(b);
+        Toast(1);
     }
 
     public void freethrowB(View view){
         b+=1;
         displayB(b);
+        Toast(1);
+    }
+
+    public void kirim(View view){
+        Intent intentKirim = new Intent (this, Main2Activity.class);
+
+        intentKirim.putExtra("SCOREA", a);
+        intentKirim.putExtra("SCOREB", b);
+        startActivity(intentKirim);
     }
 }
